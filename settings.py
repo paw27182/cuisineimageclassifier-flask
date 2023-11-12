2d4fe507955768ac5546a36d08cad79beee8f832
paw27182 <paw27182@gmail.com>
2023-11-12 17:43:24 +0900

    Python 3.11.4 -> 3.11.6

--- settings.py
+++ settings.py
@@ -3,13 +3,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 # Windows
-PYTHON_EXE_FILE = r"C:/Python/envimageclassifier/Scripts/python.exe"  # specify python executable file
+PYTHON_EXE_FILE = r"C:/Python/envcic/Scripts/python.exe"  # specify python executable file
 HOST = "127.0.0.1"  # localhost
 PORT = 8000
 
 # # Linux
-# PYTHON_EXE_FILE = "/home/paw/envimageclassifier/bin/python3.10"  # specify python executable file
+# PYTHON_EXE_FILE = "/home/paw/envcic/bin/python3.10"  # specify python executable file
 # HOST = "127.0.0.1"
 # PORT = 8000
 
-MODEL_PATH = Path(BASE_DIR, "appmain", "model", "best_model_2.13.0.h5")
+MODEL_PATH = Path(BASE_DIR, "appmain", "model", "best_model_2.14.0.h5")
